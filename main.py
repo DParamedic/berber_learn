f499547fe21e0002a0e91c6857313440b3fffbe6
Mr_Ga1ahad <gr_ku.ba@mail.ru>
2025-01-30 22:52:37 +0700

    add class EndPoint, clear func, engine func is over.

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ import os
 import win32con, win32api
 from sys import platform
 
-
-file_name = "./.txt_lib/"
-
+class EndPoints:
+    def __init__(self, struct: int, expansion='.txt'):
+        self.expansion = expansion
+        self.struct = struct
+        self = struct, expansion
+    def normal_view(self):
+        return str(self.struct) + self.expansion
+    
 def directory_find(path):
     if not os.path.exists(path):
         os.mkdir(path)
         if platform == "win32":
             win32api.SetFileAttributes(path, win32con.FILE_ATTRIBUTE_HIDDEN)
+            return True
         elif platform == "linux" or platform == "linux2":
-            continue
+            return  True
         else:
-            exit(1)
-
-def txt_reader(path):
-    max_value = int(path.split('/')[-1].split('.')[0])
+            return False
+    else:
+        return True
+    
+def txt_reader(path, end_point):
+    max_value = end_point.struct
     word_dict = {}
     down_dict = {}
     boost_dict = {}
+    new_path = path + end_point.normal_view()
     
-    with open(path, 'r') as read_line:
+    with open(new_path, 'r') as read_line:
         word_dict = {i:[j, n] for i, j, n in [line.split('/') for line in read_line]}
         
         for i in word_dict:
@@ -47,8 +56,43 @@ def txt_reader(path):
                 
     return word_dict, down_dict, boost_dict
 
-def enine():
-    end_point_list = ["./.txt_lib/", "1.txt", '2.txt', '4.txt', '8.txt', '16.txt', '32.txt', '64.txt', '128.txt', '256.txt', '512.txt', '1024.txt', '2048.txt']
-    for i in end_point_list[1:]:
-        word_dict, down_dict, boost_dict = txt_reader(end_point_list[0]+i)
-        # writer
+def end_point_generator(limiter: int=12, start: int=1, degree: int=2):
+    for i in range(limiter):
+        yield EndPoints(start)
+        start *= degree
+            
+def txt_clear(path, end_point):
+    new_path = path + end_point.normal_view()
+    
+    with open(new_path, 'w') as write_line:
+        write_line.write('')
+
+def txt_writer(path, end_point, inp_dict):
+    pass
+
+def engine():
+    f_end_point = "./.txt_lib/"
+    if directory_find(f_end_point):
+        global_down_dict, global_boost_dict = {}, {}
+        
+        for i in end_point_generator():
+            word_dict, down_dict, boost_dict = txt_reader(f_end_point, i)
+            txt_clear(f_end_point, i)
+            
+            if i.struct != 1:
+                global_down_dict.update(down_dict)
+                txt_writer(f_end_point, i, word_dict.update(global_boost_dict))
+                
+                # word_dict.clear()
+                global_boost_dict.clear()
+                # down_dict.clear()
+                
+                global_boost_dict.update(boost_dict)
+                
+            else:
+                global_down_dict.update(down_dict)
+                global_down_dict.update(word_dict)
+                global_boost_dict.update(boost_dict)
+        else:
+            txt_writer(f_end_point, EndPoints(1), global_down_dict)
+            
