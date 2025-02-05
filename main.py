70ee0b56e9112a3ac137835df4e56436dc7e1b7f
Mr_Ga1ahad <gr_ku.ba@mail.ru>
2025-02-06 00:20:25 +0700

    Code writing is over. Testing & relis now.

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def cmd_runner(path: Union[str, path_to_file]) -> None:
             break
         else:
             new_word_writer(path, cmd_status)
-    overwriting(path)
+    overwriting(path, EndPoints(0), EndPoints(1))
     
 def engine(path: Union[str, path_to_file]) -> None:
     global_down_dict, global_boost_dict = {}, {}
@@ -89,6 +89,10 @@ def engine(path: Union[str, path_to_file]) -> None:
         txt_writer(path, EndPoints(1), global_down_dict)
 
 
+def txt_read_and_split(path: Union[str, path_to_file]) -> list[list[str]]:
+    with open(path, 'r', encoding='utf-8') as read_line:
+        return [line.rstrip().split('/') for line in  read_line.readlines()]
+    
 def txt_reader(path: Union[str, path_to_file],
                end_point: end_point_type) -> tuple[dict]:
     max_value = end_point.struct
@@ -98,29 +102,27 @@ def txt_reader(path: Union[str, path_to_file],
     new_path = path + end_point.normal_view()
     
     if os.path.exists(new_path):
-        with open(new_path, 'r', encoding='utf-8') as read_line:
-            tmp_dict = {i:[j, int(n)] for i, j, n \
-                in [line.split('/') for line in read_line]}
-            
-            for i in tmp_dict:
-                if tmp_dict[i][1] == 0:
-                    input_translatte = input(f'Translate {i} is: ')
-                    if input_translatte == tmp_dict[i][0]:
-                        print('Прекрасно!')
-                        tmp_dict[i][1] = max_value*2
-                    else:
-                        print(f'Нет. Правильно: {tmp_dict[i][0]}')
-                        tmp_dict[i][1] = None
-                else: tmp_dict[i][1] -= 1
-                
-            for i in tmp_dict:
-                if tmp_dict[i][1] is None:
-                    down_dict[i] = tmp_dict[i][0], 1
-                elif tmp_dict[i][1] > max_value:
-                    boost_dict[i] = tmp_dict[i]
+        tmp_dict = {i:[j, int(n)] for i, j, n in txt_read_and_split(new_path)}
+        
+        for i in tmp_dict:
+            if tmp_dict[i][1] == 0:
+                input_translatte = input(f'Translate {i} is: ')
+                if input_translatte == tmp_dict[i][0]:
+                    print('Прекрасно!')
+                    tmp_dict[i][1] = max_value*2
                 else:
-                    word_dict[i] = tmp_dict[i]
-                    
+                    print(f'Нет. Правильно: {tmp_dict[i][0]}')
+                    tmp_dict[i][1] = None
+            else: tmp_dict[i][1] -= 1
+            
+        for i in tmp_dict:
+            if tmp_dict[i][1] is None:
+                down_dict[i] = tmp_dict[i][0], 1
+            elif tmp_dict[i][1] > max_value:
+                boost_dict[i] = tmp_dict[i]
+            else:
+                word_dict[i] = tmp_dict[i]
+                
         return word_dict, down_dict, boost_dict
     else: 
         return {}, {}, {}
@@ -128,9 +130,9 @@ def txt_reader(path: Union[str, path_to_file],
 def txt_writer(path: Union[str, path_to_file],
                end_point: end_point_type, inp_dict: dict) -> None:
     new_path = path + end_point.normal_view()
-    with open(new_path, 'w', encoding='utf-8') as read_line:
+    with open(new_path, 'w', encoding='utf-8') as write_line:
         for item in inp_dict:
-            read_line.write(f'{item}/{inp_dict[item][0]}/\
+            write_line.write(f'{item}/{inp_dict[item][0]}/\
                 {inp_dict[item][1]}\n')
 
 def txt_clear(path: Union[str, path_to_file],
@@ -141,14 +143,13 @@ def txt_clear(path: Union[str, path_to_file],
         write_line.write('')
 
 def overwriting(path: Union[str, path_to_file],
-                end_point_1: end_point_type=EndPoints(0),
-                end_point_2: end_point_type=EndPoints(1)) -> None:
+                end_point_1: end_point_type,
+                end_point_2: end_point_type) -> None:
     new_path_1 = path + end_point_1.normal_view()
     new_path_2 = path + end_point_2.normal_view()
     with open(new_path_1, 'r', encoding='utf-8') as read_line, \
             open(new_path_2, 'a', encoding='utf-8') as write_line:
-        for line in read_line:
-            write_line.write(line)
+        write_line.write(read_line.readlines())
     txt_clear(path, end_point_1)
 
 def new_word_writer(path: Union[str, path_to_file], word: str) -> None:
@@ -159,8 +160,32 @@ def new_word_writer(path: Union[str, path_to_file], word: str) -> None:
             write_line.write(f'{word.capitalize()}/\
                 {translation.capitalize()}/1\n')
 
-def word_changer():
-    pass
+def word_changer(path: Union[str, path_to_file], index: int, *, count: int=None) -> None:
+    cmd_status = input('What you do with this word? [del/change]')
+    
+    if cmd_status == 'del' or cmd_status == 'd':
+        with open(path, 'r', encoding='utf-8') as read_line, \
+                open(path, 'w', encoding='utf-8') as write_line:
+            write_line.write(read_line.readlines()[:index]
+                             + read_line.readlines()[index+1:])
+            
+    elif cmd_status == 'change' or cmd_status == 'c':
+        change_str = f'{input('Word: ')}/{input('Translate: ')}/{count}\n'
+        with open(path, 'r', encoding='utf-8') as read_line, \
+                open(path, 'w', encoding='utf-8') as write_line:
+            write_line.write(read_line.readlines()[:index]
+                             + change_str
+                             + read_line.readlines()[index+1:])
+            
 
+def word_finder(path: Union[str, path_to_file], word: str) -> int:
+    for i in end_point_generator():
+        new_path = path + i.normal_view()
+        spliting_file = txt_read_and_split(new_path)
+        
+        for line in spliting_file:
+            if line[0] == word or line[1] == word:
+                return spliting_file.index(line)
+                        
 if __name__ == '__main__':
     main()
