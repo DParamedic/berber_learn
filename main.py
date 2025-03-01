f2445e20bb0994c8f6f3d1a51d17c92d57ea4491
Mr_Ga1ahad <gr_ku.ba@mail.ru>
2025-03-01 10:10:26 +0700

    Продолжение в ооп

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ class EndPoints:
             start *= degree
 
 class UseFile:
-    def __init__(self, path: str, end_point: EndPoints):
-        self.path = path
+    def __init__(self, end_point: EndPoints):
+        self._path = './.txt_lib/'
         self.end_point = end_point
         
     def __str__(self):
@@ -34,7 +34,14 @@ class UseFile:
     def normal_view(self):
         return self.path + self.end_point.normal_view()
     
+    @property.getter
+    def get_path(self):
+        return self._path
     
+    @property.setter
+    def set_path(self, new_path):
+        self._path = new_path
+        
     def txt_read_and_split(self) -> list[list[str]]:
         """Превращает записи из файлов в списки значений."""
         with open(self.normal_view(), 'r', encoding='utf-8') as read_line:
@@ -100,7 +107,49 @@ class UseFile:
         if translation != '':
             with open(self.normal_view(), 'a', encoding='utf-8') as write_line:
                 write_line.write(f'{word.capitalize()}/{translation.capitalize()}/1\n')
-
+    
+    
+    
+    @staticmethod
+    def directory_find(_path) -> bool:
+        """Проверка, что это linux или windows.
+        И на присутствие скрытой директории,
+        если не найдена -- создание.
+        """
+        if not os.path.exists(_path):
+            os.mkdir(_path)
+            if platform == "win32":
+                win32api.SetFileAttributes(_path, win32con.FILE_ATTRIBUTE_HIDDEN)
+                return True
+            elif platform == "linux" or platform == "linux2":
+                return  True
+            else:
+                return False
+        else:
+            return True
+        
+    @classmethod
+    def word_finder(cls, path: str, word: str) -> Generator[Any, Any, None]:
+        """Поиск слова по всем файлам.
+        Как само слово, так и перевод.
+        """
+        word = word.capitalize()
+        for i in EndPoints(0), *EndPoints.end_point_generator():
+            yield cls(i)
+            new_path: str = path + i.normal_view()
+            spliting_file = txt_read_and_split(new_path)
+            
+            for line in spliting_file:
+                if line[0] == word or line[1] == word:
+                    yield from (
+                        new_path,
+                        spliting_file.index(line), 
+                        int(line[2]),
+                        )
+                else:
+                    return
+        else:
+            print(f'{word} word not exist.')
 
 
 
@@ -114,23 +163,6 @@ def main() -> None:
     f_end_point: str = "./.txt_lib/"
     if directory_find(f_end_point):
         cmd_runner(f_end_point)
-
-def directory_find(path: str) -> bool:
-    """Проверка, что это linux или windows.
-    И на присутствие скрытой директории,
-    если не найдена -- создание.
-    """
-    if not os.path.exists(path):
-        os.mkdir(path)
-        if platform == "win32":
-            win32api.SetFileAttributes(path, win32con.FILE_ATTRIBUTE_HIDDEN)
-            return True
-        elif platform == "linux" or platform == "linux2":
-            return  True
-        else:
-            return False
-    else:
-        return True
 
 def cmd_runner(path: str) -> None:
     """Запускает нечто вроде командной строки.
@@ -201,79 +233,6 @@ def engine(path: str) -> None:
         txt_writer(path, EndPoints(1), global_down_dict)
 
 
-def txt_read_and_split(path: str) -> list[list[str]]:
-    """Превращает записи из файлов в списки значений."""
-    with open(path, 'r', encoding='utf-8') as read_line:
-        return [line.rstrip('\n').split('/') for line in  read_line.readlines()]
-
-def txt_reader(path: str, end_point: EndPoints) -> Generator[dict[str, list], dict, None]:
-    """Из названия функции не очень ясно, но она так же важна,
-    как и engine. Здесь происходит чтение данных, 
-    сортровка, отправка в engine.
-    """
-    max_value: int = end_point.struct
-    word_dict: dict = {}
-    down_dict: dict = {}
-    boost_dict: dict = {}
-    new_path: str = path + end_point.normal_view()
-
-    if os.path.exists(new_path):
-        tmp_dict: dict[str, list] = {i:[j, int(n)] for i, j, n in txt_read_and_split(new_path)}
-
-        for i in tmp_dict:
-            if tmp_dict[i][1] == 0:
-                input_translatte = input(f'Translate {i} is: ').capitalize()
-                if input_translatte == tmp_dict[i][0]:
-                    print('Прекрасно!')
-                    tmp_dict[i][1] = max_value*2
-                else:
-                    print(f'Нет. Правильно: {tmp_dict[i][0]}')
-                    tmp_dict[i][1] = None
-            else: tmp_dict[i][1] -= 1
-
-        for i in tmp_dict:
-            if tmp_dict[i][1] is None:
-                down_dict[i] = [tmp_dict[i][0], 1]
-            elif tmp_dict[i][1] > max_value:
-                boost_dict[i] = tmp_dict[i]
-            else:
-                word_dict[i] = tmp_dict[i]
-
-        yield from (word_dict, down_dict, boost_dict)
-    else: 
-        yield from ({}, {}, {})
-
-def txt_writer(path: str, end_point: EndPoints, inp_dict: dict) -> None:
-    """Запись в файл."""
-    new_path: str = path + end_point.normal_view()
-    with open(new_path, 'w', encoding='utf-8') as write_line:
-        for item in inp_dict:
-            write_line.write(f'{item}/{inp_dict[item][0]}/{inp_dict[item][1]}\n')
-
-def txt_clear(path: str, end_point: EndPoints) -> None:
-    """Очистка файла."""
-    new_path: str = path + end_point.normal_view()
-
-    with open(new_path, 'w') as write_line:
-        write_line.write('')
-
-def overwriting(path: str, end_point_1: EndPoints, end_point_2: EndPoints) -> None:
-    """Переписывание одного файла полностью в другой."""
-    new_path_1: str = path + end_point_1.normal_view()
-    new_path_2: str = path + end_point_2.normal_view()
-    with open(new_path_1, 'r', encoding='utf-8') as read_line, \
-            open(new_path_2, 'a', encoding='utf-8') as write_line:
-        write_line.write(read_line.read())
-    txt_clear(path, end_point_1)
-
-def new_word_writer(path: str, word: str) -> None:
-    """"Функция записи новых ппар слов в словарь."""
-    new_path: str = path + EndPoints(0).normal_view()
-    translation: str = input('Перевод: ')
-    if translation != '':
-        with open(new_path, 'a', encoding='utf-8') as write_line:
-            write_line.write(f'{word.capitalize()}/{translation.capitalize()}/1\n')
-
 def word_changer(path: str, index: int, count: Optional[int] = None) -> None:
     """Функции изменения вынесены на особицу,
     они не вызываются из engine.
@@ -326,4 +285,5 @@ def word_finder(path: str, word: str) -> Generator[Any, Any, None]:
         print(f'{word} word not exist.')
 
 if __name__ == '__main__':
+    st = UseFile()
     main()
