832ee0f38bdf92ce9adfd128089bda7fd7d70090
Mr_Ga1ahad <gr_ku.ba@mail.ru>
2025-06-09 12:46:53 +0700

    docs: add docstrings for database models

--- app/dictionary/models.py
+++ app/dictionary/models.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, varchar255, varchar31
 
 class Dictionary(Base):
-    """
-    Модель для хранения словарей пользователя
+    """Модель для хранения словарей пользователя.
 
     Attributes:
         id (int): уникальный идентификатор словаря
         user_id (int): id пользователя
+        language_id (int): id языковой модели
     """
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id = mapped_column(ForeignKey('user.id', ondelete='cascade'))
@@ -25,20 +25,26 @@ class Dictionary(Base):
     )
 
 class Language(Base):
-    """
-    Модель для хранения списка используемых языков
+    """Модель для хранения списка пар языков, применяемых при идентификации словаря.
 
     Attributes:
         id (int): уникальный идентификатор языка
-        name (str): название языка
+        main_language (str): название основного языка
+        translation_language (str): название языка переводов
     """
     id: Mapped[int] = mapped_column(primary_key=True)
     main_language: Mapped[varchar31]
     translation_language: Mapped[varchar31]
     
-    dictionary: Mapped['Dictionary'] = relationship(back_populates='language', uselist=False)
+    dictionaries: Mapped['Dictionary'] = relationship(back_populates='language', uselist=False)
 
 class Word(Base):
+    """Модель для хранения слов или переводов.
+
+    Attributes:
+        id (int): уникальный идентификатор слова
+        content (str): содержимое слова
+    """
     id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[varchar31] = mapped_column(index=True)
     
@@ -46,28 +52,65 @@ class Word(Base):
     translations_association: Mapped[list['Word_Translate']] = relationship(back_populates='word')
     
 class Note(Base):
+    """Модель для хранения заметок.
+
+    Attributes:
+        id (int): уникальный идентификатор заметки
+        content (str): содержимое заметки
+    """
     id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[varchar255]
 
     word_translate: Mapped['Word_Translate'] = relationship(back_populates='note', uselist=False)
+    word_translations: Mapped['Word_Translate'] = relationship(back_populates='note')
     
 class Word_Translate(Base):
+    """Модель для связи слова с его переводом/переводами.
+
+    Attributes:
+        word_id (int): id слова
+        translate_id (int): id перевода
+        note_id (int): id заметки
+        dict_id (int): id словаря
+        page_id (int): id длительности пребывания слова в таблице
+        count (int): длительность пребывания по слова в page по факту
+    """
     word_id = mapped_column(ForeignKey('word.id', ondelete='cascade'))
-    translate_id = mapped_column(ForeignKey('word.id', ondelete='cascade'),
-                                   CheckConstraint('second_word_id != word_id', name='check_second_word_id_not_eq_word_id'))
-    note_id: Mapped[int | None] = mapped_column(ForeignKey('note.id', ondelete='restrict'))
+    translate_id = mapped_column(
+        ForeignKey(
+            'word.id', ondelete='cascade'
+            ),CheckConstraint(
+                'second_word_id != word_id',
+                name='check_second_word_id_not_eq_word_id'
+                )
+            )
+    note_id: Mapped[int | None] = mapped_column(
+        ForeignKey('note.id', ondelete='restrict')
+        )
     dict_id = mapped_column(ForeignKey('dictionary.id', ondelete='cascade'))
     page_id = mapped_column(ForeignKey('page.id'))
-    count: Mapped[int] = mapped_column(CheckConstraint('count < page_value', name='check_count_less_page'))
+    count: Mapped[int] = mapped_column(
+        CheckConstraint('count < page_value', name='check_count_less_page')
+        )
     
-    dictionary: Mapped['Dictionary'] = relationship(back_populates='word_translate', uselist=False)
-    words: Mapped[list['Word']] = relationship(back_populates='word_association')
-    translations: Mapped[list['Word']] = relationship(back_populates='translations_association')
-    note: Mapped['Note'] = relationship(back_populates='word_translate', uselist=False)
-    page = relationship('Page', back_populates='word_translates', uselist=False)
+    dictionary: Mapped['Dictionary'] = relationship(
+        back_populates='word_translate', uselist=False)
+    words: Mapped[list['Word']] = relationship(
+        back_populates='word_association')
+    translations: Mapped[list['Word']] = relationship(
+        back_populates='translations_association')
+    note: Mapped['Note'] = relationship(
+        back_populates='word_translate', uselist=False)
+    page = relationship(
+        'Page', back_populates='word_translates', uselist=False)
 
     __table_args__ = (
-        PrimaryKeyConstraint('word_id', 'dict_id', 'translate_id', name='word_id_dict_id_primary_key'),
+        PrimaryKeyConstraint(
+            'word_id',
+            'dict_id',
+            'translate_id',
+            name='word_id_dict_id_primary_key'
+            ),
     )
 
 
--- app/settings/models.py
+++ app/settings/models.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, varchar31
 
 class Page(Base):
+    """Модель для хранения продолжительности интервалов.
+
+    Attributes:
+        id (int): Уникальный идентификатор интервала
+        length (int): Длительность интервала
+    """
+    # Interval
     id: Mapped[int] = mapped_column(primary_key=True)
     length: Mapped[int] = mapped_column(unique=True)
     
@@ -11,6 +18,17 @@ class Page(Base):
     word_translates = relationship('Word_Translate', back_populates='page')
 
 class Book(Base):
+    """Модель для хранения списков интервалов.
+    
+    Attributes:
+        id (int): Уникальный идентификатор списка
+        name (str): Наименование списка
+
+    Rules:
+        1. Базовый -- classic.
+        2. Если нет причин, то остальные -- m<N>, где N := 1,2...inf
+    """
+    # List
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[varchar31] = mapped_column(unique=True)
 
@@ -18,6 +36,13 @@ class Book(Base):
     users_settings: Mapped['User_Settings'] = relationship(back_populates='page')
 
 class Page_Book(Base):
+    """Модель для связи списков с их интервалами.
+
+    Attributes:
+        book_id (int): id списка
+        length_id (int): id интервала
+    """
+    # Interval_List
     book_id = mapped_column(ForeignKey('book.id', ondelete='cascade'))
     length_id = mapped_column(ForeignKey('page.id', ondelete='cascade'))
     
@@ -28,6 +53,12 @@ class Page_Book(Base):
         PrimaryKeyConstraint('book_id', 'length_id', name='book_id_length_id_primary_key'),
     )
 class User_Settings(Base):
+    """Модель для хранения пользовательских настроек.
+    
+    Attributes:
+        user_id (int): id пользователя
+        book_id (int): id списка
+    """
     user_id = mapped_column(ForeignKey('user.id', ondelete='cascade'))
     book_id = mapped_column(ForeignKey('book.id', ondelete='cascade'))
 
