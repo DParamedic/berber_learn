f2035edaf9a158329b52aeb10e376e4ccb585474
Mr_Ga1ahad <gr_ku.ba@mail.ru>
2025-06-09 13:39:20 +0700

    refactor: rename models

--- app/settings/models.py
+++ app/settings/models.py
@@ -3,21 +3,20 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base, varchar31
 
-class Page(Base):
+class Interval(Base):
     """Модель для хранения продолжительности интервалов.
 
     Attributes:
         id (int): Уникальный идентификатор интервала
         length (int): Длительность интервала
     """
-    # Interval
     id: Mapped[int] = mapped_column(primary_key=True)
     length: Mapped[int] = mapped_column(unique=True)
     
-    page_books: Mapped['Page_Book'] = relationship(back_populates='page')
-    word_translates = relationship('Word_Translate', back_populates='page')
+    page_books: Mapped['Link_Interval_List'] = relationship(back_populates='interval')
+    word_translates = relationship('Word_Translate', back_populates='interval')
 
-class Book(Base):
+class Interval_List(Base):
     """Модель для хранения списков интервалов.
     
     Attributes:
@@ -28,29 +27,28 @@ class Book(Base):
         1. Базовый -- classic.
         2. Если нет причин, то остальные -- m<N>, где N := 1,2...inf
     """
-    # List
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[varchar31] = mapped_column(unique=True)
 
-    page_books: Mapped['Page_Book'] = relationship(back_populates='book')
-    users_settings: Mapped['User_Settings'] = relationship(back_populates='page')
+    page_books: Mapped['Link_Interval_List'] = relationship(back_populates='interval_list')
+    users_settings: Mapped['User_Settings'] = relationship(back_populates='interval_list')
 
-class Page_Book(Base):
+class Link_Interval_List(Base):
     """Модель для связи списков с их интервалами.
 
     Attributes:
         book_id (int): id списка
         length_id (int): id интервала
     """
-    # Interval_List
-    book_id = mapped_column(ForeignKey('book.id', ondelete='cascade'))
-    length_id = mapped_column(ForeignKey('page.id', ondelete='cascade'))
+    # link_interval_list
+    interval_list_id = mapped_column(ForeignKey('interval_list.id', ondelete='cascade'))
+    interval_id = mapped_column(ForeignKey('interval.id', ondelete='cascade'))
     
-    books: Mapped['Book'] = relationship(back_populates='page_book')
-    pages: Mapped['Page'] = relationship(back_populates='page_book')
+    books: Mapped['Interval_List'] = relationship(back_populates='link_interval_list')
+    pages: Mapped['Interval'] = relationship(back_populates='link_interval_list')
     
     __table_args__ = (
-        PrimaryKeyConstraint('book_id', 'length_id', name='book_id_length_id_primary_key'),
+        PrimaryKeyConstraint('interval_list_id', 'interval_id', name='pk_interval_list_id_interval_id'),
     )
 class User_Settings(Base):
     """Модель для хранения пользовательских настроек.
@@ -60,11 +58,11 @@ class User_Settings(Base):
         book_id (int): id списка
     """
     user_id = mapped_column(ForeignKey('user.id', ondelete='cascade'))
-    book_id = mapped_column(ForeignKey('book.id', ondelete='cascade'))
+    interval_list_id = mapped_column(ForeignKey('interval_list.id', ondelete='cascade'))
 
     user = relationship('User', back_populates='user_settings', uselist=False)
-    book: Mapped['Book'] = relationship(back_populates='user_settings', uselist=False)
+    book: Mapped['Interval_List'] = relationship(back_populates='user_settings', uselist=False)
 
     __table_args__ = (
-        PrimaryKeyConstraint('user_id', 'book_id', name='user_id_book_id_primary_key'),
+        PrimaryKeyConstraint('user_id', 'interval_list_id', name='pk_user_id_interval_list_id'),
     )
